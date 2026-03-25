a4626a9768686717caa8b87aaa38df6ac7bd5246
za-beep-sudo <zabeepsudo@gmail.com>
2026-03-25 19:59:44 +0500

    code pushed for new changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -13,3 +14,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 
+Route::get('/search', function () {
+    $query = 'Best backend framework';
+
+    $embedding = Str::of($query)->toEmbeddings();
+
+    $documents = DB::table('documents')
+        ->whereVectorSimilarTo('embedding', $embedding)
+        ->limit(5)
+        ->get();
+
+    return $documents;
+});
